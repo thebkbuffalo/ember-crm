0d5a2fd412be29617e69cc103cac6f56928356c6
Evan Berg <evan.m.berg@gmail.com>
2015-09-15 17:48:54 -0400

    added ember things

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 
+//= require jquery
+//= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+//= require_self
+//= require ./app
+
+// for more details see: http://emberjs.com/guides/application/
+App = Ember.Application.create();
+
 //= require_tree .
